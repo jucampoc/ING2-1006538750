9a704e5eb70cfba34b856a0a13aa8f39875f5017
jucampoc <jucampoc@unal.edu.co>
2026-03-31 21:02:15 -0500

    feat(pacientes): crear base de datos, modelo, controlador y rutas API

---
+++ services/laravel-patient-service/database/migrations/2026_04_01_014915_create_patients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('identityd_document')->unique();
+            $table->date('birthday');
+            $table->string('phone')->nullable();
+            $table->string('blood_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
