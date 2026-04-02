cb79114da273664b33c0de239e43bdbb3f97f4a2
jucampoc <jucampoc@unal.edu.co>
2026-04-02 00:42:56 -0500

    feat(seeders) implementacion de seeders para mejor visualizacion de funcionamiento

--- services/laravel-patient-service/routes/api.php
+++ services/laravel-patient-service/routes/api.php
@@ -5,4 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
 
 
-Route::apiResource('patients', PatientController::class);
+Route::middleware('auth.secret')->group(function () {
+    Route::get('/patients', [PatientController::class, 'index']);
+    Route::post('/patients', [PatientController::class, 'store']);
+    Route::get('/patients/{id}', [PatientController::class, 'show']);
+    Route::put('/patients/{id}', [PatientController::class, 'update']);
+    Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
+});
