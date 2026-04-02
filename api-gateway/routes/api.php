b69b91ca34a25387cdba5f07024638fddf589624
jucampoc <jucampoc@unal.edu.co>
2026-04-01 19:43:24 -0500

    feat: CRUD completo y seguridad por token implementada en PatientProxy

--- api-gateway/routes/api.php
+++ api-gateway/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PatientProxyController;
 use App\Http\Controllers\AppointmentProxyController;
+use App\Http\Controllers\MedicalRecordProxyController;
 
 // Rutas públicas
 Route::post('/login', [AuthController::class, 'login']);
@@ -26,6 +27,9 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/patients', [PatientProxyController::class, 'index']);
     Route::post('/patients', [PatientProxyController::class, 'store']);
+    Route::get('/patients/{id}', [PatientProxyController::class, 'show']);
+    Route::put('/patients/{id}', [PatientProxyController::class, 'update']);
+    Route::delete('/patients/{id}', [PatientProxyController::class, 'destroy']);
 
     Route::get('/appointments', [AppointmentProxyController::class, 'index']);
     Route::post('/appointments', [AppointmentProxyController::class, 'store']);
@@ -33,7 +37,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/appointments/{id}', [AppointmentProxyController::class, 'destroy']);
 
 
-    Route::get('/medical-records', [\App\Http\Controllers\MedicalRecordProxyController::class, 'index']);
-    Route::post('/medical-records', [\App\Http\Controllers\MedicalRecordProxyController::class, 'store']);
+    Route::get('/medical-records', [MedicalRecordProxyController::class, 'index']);
+    Route::post('/medical-records', [MedicalRecordProxyController::class, 'store']);
+    Route::get('/medical-records/{id}', [MedicalRecordProxyController::class, 'show']);
+    Route::put('/medical-records/{id}', [MedicalRecordProxyController::class, 'update']);
+    Route::delete('/medical-records/{id}', [MedicalRecordProxyController::class, 'destroy']);
+
 
 });
