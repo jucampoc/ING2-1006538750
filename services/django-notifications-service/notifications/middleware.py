43874a8b58429efd5daa5516dea71760aff47a6f
jucampoc <jucampoc@unal.edu.co>
2026-05-12 22:34:16 -0500

    fix: correcion de seguridad al hardcordear variables de entorno en codigo

--- services/django-notifications-service/notifications/middleware.py
+++ services/django-notifications-service/notifications/middleware.py
@@ -1,10 +1,14 @@
+import os
 from django.http import JsonResponse
 
 class InternalTokenMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.token = os.environ.get('TOKEN_SECRETO')
+        if not self.token:
+            raise ValueError("La variable de entorno TOKEN_SECRETO no está definida.")
 
     def __call__(self, request):
-        if request.headers.get('Authorization') != 'Token miclave123':
+        if request.headers.get('Authorization') != f'Token {self.token}':
             return JsonResponse({'error': 'No autorizado'}, status=403)
         return self.get_response(request)
